167fc6d2365e5668b3ac041b7614234301eaa166
Kamil Osak <32075064+kmlhombre@users.noreply.github.com>
2021-02-23 20:33:07 +0100

    Add client and reformat project structure

---
+++ ServerEchoLibrary/Klient/Klient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Klient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte[] message = new byte[1024];
+            TcpClient client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000));
+            NetworkStream stream = client.GetStream();
+            string line;
+            string converted;
+
+            while (true)
+            {
+                stream.Read(message, 0, message.Length);
+                converted = Encoding.ASCII.GetString(message, 0, message.Length);
+                if (converted == "Q")
+                {
+                    break;
+                }
+                Console.Write(converted);
+
+                if (stream.DataAvailable == false)
+                {
+                    Console.Write("\n");
+                    line = Console.ReadLine();
+                    message = new ASCIIEncoding().GetBytes(line);
+                    client.GetStream().Write(message, 0, message.Length);
+                    Array.Clear(message, 0, message.Length);
+                    converted = "";
+                }
+            }
+        }
+    }
+}
+
